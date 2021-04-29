3df9304eb45f8261e4cd194ddeda4f1d66c70ce9
fumi-y <0901fumito@gmail.com>
2021-04-29 19:26:28 +0900

    テーブル作成

--- db/migrate/20210429101311_devise_create_users.rb
+++ db/migrate/20210429101311_devise_create_users.rb
@@ -6,6 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :profile,            null: false
 
       ## Recoverable
       t.string   :reset_password_token
