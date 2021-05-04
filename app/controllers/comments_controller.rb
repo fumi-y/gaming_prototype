5a8ae2d6791f57e32da552b5820a539a8994b992
fumi-y <0901fumito@gmail.com>
2021-05-04 17:14:06 +0900

    ストロングパラメーターの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,10 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, prototype_id: prototype(params[:id]))
+  end
 end
