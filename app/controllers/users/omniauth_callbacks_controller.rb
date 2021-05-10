8a584a2c706daca5eb18a86c8b7737676505d190
fumi-y <0901fumito@gmail.com>
2021-05-10 15:43:17 +0900

    ユーザー情報の有無で分岐させる

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,5 +7,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
   def authorization
     @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      render template: 'devise/registrations/new'
+    end
   end
 end
