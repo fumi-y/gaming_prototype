ac8facab1ecbb345d2cab4c25969f5996dc1a5c8
fumi-y <0901fumito@gmail.com>
2021-05-10 15:17:08 +0900

    deviseでOumniAuthが使用できる
    アソシエーションも記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   validates :nickname, presence: true
   validates :profile, presence: true
 
   has_many :prototypes
   has_many :comments, dependent: :destroy
+  hasmany :sns_credentials
 end
